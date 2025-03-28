508d12dcd59bcdee729ad37b6f1665cf4e3e2ec0
APIVerve Team <hello@apiverve.com>
2025-03-28 17:25:35 -0500

    Bug Fixes

---
+++ src/apiverve_humannameparser/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_humannameparser',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Human Name Parser is a simple tool for parsing human names into their components. It returns the parsed components of the human name.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
